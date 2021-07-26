868ef81e1e680ff813bb4e76901686bac93fdb71
aifuturecraft <guowj12@126.com>
2021-07-26 14:12:27 +0800

    Add comment for samples

--- learning_rate_adjustment.py
+++ learning_rate_adjustment.py
@@ -15,3 +15,47 @@ for i in range(10):
 
 plt.plot(range(10),lrs)
 plt.show()
+#----------
+model = torch.nn.Linear(2, 1)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+lmbda = lambda epoch: 0.65 ** epoch
+scheduler = torch.optim.lr_scheduler.MultiplicativeLR(optimizer, lr_lambda=lmbda)
+lrs = []
+
+for i in range(10):
+    optimizer.step()
+    lrs.append(optimizer.param_groups[0]["lr"])
+#     print("Factor = ",0.95," , Learning Rate = ",optimizer.param_groups[0]["lr"])
+    scheduler.step()
+
+plt.plot(range(10),lrs)
+plt.show()
+#--------
+model = torch.nn.Linear(2, 1)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=2, gamma=0.1)
+lrs = []
+
+for i in range(10):
+    optimizer.step()
+    lrs.append(optimizer.param_groups[0]["lr"])
+#     print("Factor = ",0.1 if i!=0 and i%2!=0 else 1," , Learning Rate = ",optimizer.param_groups[0]["lr"])
+    scheduler.step()
+
+plt.plot(range(10),lrs)
+plt.show()
+#------
+model = torch.nn.Linear(2, 1)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10, eta_min=0)
+lrs = []
+
+
+for i in range(100):
+    optimizer.step()
+    lrs.append(optimizer.param_groups[0]["lr"])
+#     print("Factor = ",i," , Learning Rate = ",optimizer.param_groups[0]["lr"])
+    scheduler.step()
+
+plt.plot(lrs)
+plt.show()
--- transforms.py
+++ transforms.py
@@ -4,21 +4,26 @@ from torchvision.transforms import functional as TF
 import torch
 import matplotlib.pyplot as plt
 
-path = "/Users/wguo/GitRepo/dl-image-classification/train_full/9inches_dirty/9inches_dirty_1.bmp"
+#path = "/Users/wguo/GitRepo/dl-image-classification/train_full/9inches_dirty/9inches_dirty_1.bmp"
+path = "/Users/wguo/GitRepo/dl-image-classification/3classes/train/9inches_dirty/9inches_dirty_1.bmp"
 img = Image.open(path)
 img
 
 transforms = transforms.Compose([
     transforms.Resize(255),
     transforms.ColorJitter(),
-    transforms.CenterCrop(200),
+    transforms.CenterCrop(224),
     #transforms.RandomCrop(224),
-    transforms.RandomHorizontalFlip(),
-    transforms.Resize(200),
+    #transforms.RandomHorizontalFlip(),
+    transforms.Resize(224),
     transforms.ToTensor(),
     transforms.ToPILImage()
 ])
 
+plt.imshow(img)
+plt.show()
+
 img = transforms(img)
+
 plt.imshow(img)
 plt.show()
